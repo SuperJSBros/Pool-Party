2af38d56d567ac665c1faadc5b2f2f959b5a1370
Bing <32265253+exomachine@users.noreply.github.com>
2022-09-19 19:38:29 -0400

    added credential examples and updated gitignore

--- index.js
+++ index.js
@@ -1,33 +1,98 @@
-require("dotenv").config(); //moved bot token to use dotenv
-// console.log(process.env);
-const Discrod = require("discord.js");
-const client = new Discrod.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
-const prefix = "!";
+const fs = require('fs').promises;
+const path = require('path');
+const process = require('process');
+const {authenticate} = require('@google-cloud/local-auth');
+const {google} = require('googleapis');
 
-client.on("messageCreate", (message) => {
-  //if the message is from a bot, ignore it
-  if (message.author.bot) return;
-  //bot on demande commande start with prefix ! , below code are always-on
-  if (!message.content.startsWith(prefix)) {
-    const regex = /wincho/;
-    if (message.toLowerCase().search(regex) >= 0 || message.toLowerCase().search(/@wincho/) >= 0) {
-      if (Math.floor(Math.random(10) === 7 )  return "bruh";
-    }
-  } else {
-    processBotCommande(message);
-  }
-});
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
 
-function processBotCommande(message) {
-  //the received message containt a commade for the bot to do (prefix !), below are bots functions
-  const commandBody = message.content.slice(prefix.length);
-  const args = commandBody.split(" ");
-  const command = args.shift().toLowerCase();
-  if (command === "ping") {
-    const timeTaken = Date.now() - message.createdTimestamp;
-    // message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
-    message.reply("Pong!");
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {Promise<OAuth2Client|null>}
+ */
+async function loadSavedCredentialsIfExist() {
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
   }
 }
 
-client.login(process.env.BOT_TOKEN);
+/**
+ * Serializes credentials to a file comptible with GoogleAUth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+async function saveCredentials(client) {
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: 'authorized_user',
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
+}
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
+
+/**
+ * Lists the next 10 events on the user's primary calendar.
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+async function listEvents(auth) {
+  const calendar = google.calendar({version: 'v3', auth});
+  const res = await calendar.events.list({
+    calendarId: '2forffm1qcfqhq86n47ihtd96c@group.calendar.google.com',
+    timeMin: (new Date()).toISOString(),
+    maxResults: 10,
+    singleEvents: true,
+    orderBy: 'startTime',
+  });
+  const events = res.data.items;
+  if (!events || events.length === 0) {
+    console.log('No upcoming events found.');
+    return;
+  }
+  console.log('Upcoming 10 events:');
+  events.map((event, i) => {
+    const start = event.start.dateTime || event.start.date;
+    console.log(`${start} - ${event.summary}`);
+  });
+}
+
+async function updateEvent(auth) {
+  
+}
+
+
+authorize().then(listEvents).catch(console.error);
