63db868effe5c0eb036a00f529f9eada001f41d0
Eric Leon <eleon@diabsolut.com>
2022-10-24 23:16:17 -0400

    feat: add db connection

---
+++ discord-bot/repository/event-repository.ts
@@ -0,0 +1,32 @@
+import { Client } from "pg";
+
+class EventRepository {
+  private dbClient?: Client = undefined;
+  constructor() {}
+  public initDbConnection():void {
+    const config = {
+      host: process.env.POSTGRES_HOST,
+      database: process.env.POSTGRES_DB,
+      port: 5432,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PW,
+    };
+    if (this.dbClient) {
+      console.log(`A connection to DB ${config.database} has already been established`);
+      return;
+    }
+
+    const client = new Client(config);
+    client
+      .connect()
+      .then(() => {
+        this.dbClient = client;
+        console.log(
+          `Connected to DB ${config.database} on port ${config.port}`
+        );
+      })
+      .catch((e) => console.error(e));
+  }
+}
+
+export const eventRepository: EventRepository = new EventRepository();
