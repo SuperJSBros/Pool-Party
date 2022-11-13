af30c1637d5fa278347aebddf5b1ecc06187b94a
Bing <32265253+exomachine@users.noreply.github.com>
2022-11-13 15:25:48 -0500

    completed list-event command

--- discord-bot/services/updater-service.ts
+++ discord-bot/services/updater-service.ts
@@ -1,12 +1,15 @@
 import { rejects } from "assert"
 import { postgress } from "../db/postgress"
 import { EmbedBuilder } from "discord.js"
+import * as dotenv from "dotenv";
+import * as path from "path";
+// init dotenv config
+dotenv.config({ path: path.join(__dirname, ".env") });
 
 /**
  * Query database for information to display in the CLI
  *
  */
-
 class UpdaterService {
     //Query db and return all upcoming events
     public async listEvent(interaction: any): Promise<any> {
@@ -18,13 +21,29 @@ class UpdaterService {
             ORDER BY event_start ASC
             `
         )
+        let organiser = await postgress.dbClient.query(
+            `
+            SELECT organiser_name 
+            FROM public.organiser
+            ORDER BY id ASC
+            `
+        );
 
         let message: string = `There are ${result.rowCount} upcoming event(s) !\n`
         for (const [key, value] of Object.entries(result.rows)) {
             console.log(`${key} : ${value.event_name}`)
-            message = [message, "\n", value.event_start.toISOString().substring(0, 10), "@", value.event_start.toISOString().substring(11, 16), " --> ", value.event_name].join(" ")
+            message = [
+                message,
+                "\n",
+                value.event_start.toISOString().substring(0, 10),
+                "@",
+                value.event_start.toISOString().substring(11, 16),
+                " --> ",
+                `[${value.event_name}](https://discord.com/channels/${process.env.SERVER_ID}/${value.message_discord_ref})`,
+                "  by",
+                organiser.rows[value.organiser_id - 1].organiser_name
+            ].join(" ")
         }
-
         this.interactionReply(interaction, message)
     }
 
