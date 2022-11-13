b5aac779a1e4b728cfb1737e3075314f8eee1ca2
Bing <32265253+exomachine@users.noreply.github.com>
2022-11-13 09:57:28 -0500

    added db query to read database

--- discord-bot/services/display-service.ts
+++ discord-bot/services/display-service.ts
@@ -1,15 +1,31 @@
-//import { postgress } from "./db/postgress";
-
 import { rejects } from "assert";
+import { postgress } from "../db/postgress";
 
 /**
- * Query database for information to display in the GUI
+ * Query database for information to display in the CLI
  *
  */
 
 class DisplayService {
     public listEvent() {
         console.log("Upcoming event are : ...");
+        this.readDatabase();
+    }
+
+    private async readDatabase() {
+        let result = await postgress.dbClient.query(
+            `
+            SELECT *
+            FROM public.event
+            WHERE event_start >= now()
+            ORDER BY event_start ASC
+            `
+        );
+        for(const [key, value] of Object.entries(result.rows)) {
+            console.log(`${key} and ${value.event_name}`);
+            //console.log(value);
+        }
+        return result;
     }
 }
 
