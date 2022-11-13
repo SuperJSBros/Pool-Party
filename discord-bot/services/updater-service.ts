f2c330714ce474c62a16b826edd588e6733c95cc
Bing <32265253+exomachine@users.noreply.github.com>
2022-11-13 14:12:20 -0500

    implemented but list-event reply

--- discord-bot/services/updater-service.ts
+++ discord-bot/services/updater-service.ts
@@ -1,6 +1,6 @@
 import { rejects } from "assert"
 import { postgress } from "../db/postgress"
-import {EmbedBuilder } from "discord.js";
+import { EmbedBuilder } from "discord.js"
 
 /**
  * Query database for information to display in the CLI
@@ -8,13 +8,8 @@ import {EmbedBuilder } from "discord.js";
  */
 
 class UpdaterService {
-    public listEvent(interaction: any) {
-        console.log("Upcoming event are : ...")
-        this.interactionReply(interaction, "test");
-        //this.readDatabase();
-    }
-
-    private async readDatabase() {
+    //Query db and return all upcoming events
+    public async listEvent(interaction: any): Promise<any> {
         let result = await postgress.dbClient.query(
             `
             SELECT *
@@ -23,29 +18,24 @@ class UpdaterService {
             ORDER BY event_start ASC
             `
         )
+
+        let message: string = `There are ${result.rowCount} upcoming event(s) !\n`
         for (const [key, value] of Object.entries(result.rows)) {
-            console.log(`${key} and ${value.event_name}`)
-            //console.log(value);
+            console.log(`${key} : ${value.event_name}`)
+            message = [message, "\n", value.event_start.toISOString().substring(0, 10), "@", value.event_start.toISOString().substring(11, 16), " --> ", value.event_name].join(" ")
         }
-        return result;
+
+        this.interactionReply(interaction, message)
     }
 
-    //interaction reply of bot messages
-    private interactionReply(interaction: any, message: string){
+    private interactionReply(interaction: any, message: string) {
         interaction.reply({
             embeds: [
-                new EmbedBuilder()
-                    .setColor("Orange")
-                    .setDescription(message),
+                new EmbedBuilder().setColor("Orange").setDescription(message),
             ],
-            ephemeral: true
-        });
-
+            ephemeral: true,
+        })
     }
 }
-
-
-
-
 
 export const updaterService: UpdaterService = new UpdaterService()
