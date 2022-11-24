b7f4171a23245c74c5dde7604de33753af5240a7
Eric Leon <eleon@diabsolut.com>
2022-11-23 19:53:01 -0500

    feat: added event change listener

--- discord-bot/index.ts
+++ discord-bot/index.ts
@@ -1,4 +1,4 @@
-import { Client, GatewayIntentBits, Interaction } from "discord.js"
+import { Client, GatewayIntentBits, GuildScheduledEvent, GuildScheduledEventStatus, Interaction, Partials } from "discord.js"
 import * as dotenv from "dotenv"
 import { postgress } from "./db/postgress"
 import { commandService } from "./services/command-service"
@@ -20,7 +20,7 @@ const client = new Client({
         GatewayIntentBits.MessageContent,
         GatewayIntentBits.GuildMessageReactions,
     ],
-    // partials: [Partials.Message, Partials.Channel, Partials.Reaction], //add this section to get additional messages info ex: emoji reactions
+    partials: [Partials.GuildScheduledEvent], //add this section to get additional messages info ex: emoji reactions
 })
 
 // *********************
@@ -49,6 +49,12 @@ client.on("interactionCreate", (interaction: Interaction) => {
             default:
                 console.log("no matching command")
         }
+    }
+})
+
+client.on("guildScheduledEventUpdate",(event:GuildScheduledEvent<GuildScheduledEventStatus> | null) =>{
+    if(event){
+        // write code here
     }
 })
 
